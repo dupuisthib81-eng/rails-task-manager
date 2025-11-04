2aac0394f9d0102cad5b9dcff133379f0598bc12
thibault dupuis <dupuis.thib81@gmail.com>
2025-11-04 22:26:00 +0100

    tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,28 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save!
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,18 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+   #root to: "posts#index"
+  # read all
+   get 'tasks', to: 'tasks#index'
+   # create(1)
+   get 'tasks/new', to: 'tasks#new', as: :new_task
+   # create(2)
+   post 'tasks', to: 'tasks#create'
+   # update
+   #get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_restaurant
+   #patch 'tasks/:id', to: 'tasks#update'
+   # delete
+   #delete 'tasks/:id', to: 'tasks#destroy'
+   # read one
+   get 'tasks/:id', to: 'tasks#show', as: :task
 end
